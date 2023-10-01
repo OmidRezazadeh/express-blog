8258e407c538e96c0ee816ce1cc41a840fd8500b
orprogrammer <orprogrammer@gmail.com>
2023-10-01 17:45:29 +0430

    start make create method

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,47 +1,50 @@
 const Blog = require("../models/Blog");
-const {truncate} = require("../utils/truncate");
+const { truncate } = require("../utils/truncate");
+const Contact = require("../models/Contact");
+const ContactValidation = require("../models/validation/ContactValidation");
 
 exports.getIndex = async (req, res) => {
-    try {
-        const posts = await Blog.find({status: 0}).sort({createdAt: "desc"});
+  try {
+    const posts = await Blog.find({ status: 0 }).sort({ createdAt: "desc" });
 
-        res.render("index", {
-            "pageTitle": "وبلاگ",
-            path: "/",
-            posts: posts,
-            truncate
-        })
-    } catch (err) {
-        console.log(err);
-        res.render("errors/500")
-    }
+    res.render("index", {
+      pageTitle: "وبلاگ",
+      path: "/",
+      posts: posts,
+      truncate,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("errors/500");
+  }
 };
 exports.getSinglePost = async (req, res) => {
-    try {
-        const post = await Blog.findOne({_id: req.params.id})
-            .populate("user")
-        console.log(post);
-        if (!post) return res.redirect("errors/404");
-        res.render("post", {
-            pageTitle: post.title,
-            path: "/post",
-            post: post
-        });
-    } catch (err) {
-        console.log(err);
-        res.render("errors/500")
-    }
-}
-exports.getContactPage=(req,res)=>{
-    res.render("contact",{
-        pageTitle:" تماس باما",
-        path:"/contact",
-        message:req.flash("success_message"),
-        error:req.flash("error"),
- 
+  try {
+    const post = await Blog.findOne({ _id: req.params.id }).populate("user");
+    console.log(post);
+    if (!post) return res.redirect("errors/404");
+    res.render("post", {
+      pageTitle: post.title,
+      path: "/post",
+      post: post,
     });
+  } catch (err) {
+    console.log(err);
+    res.render("errors/500");
+  }
+};
+exports.getContactPage = (req, res) => {
+  res.render("contact", {
+    pageTitle: " تماس باما",
+    path: "/contact",
+    message: req.flash("success_message"),
+    error: req.flash("error"),
+  });
 
-exports.handlerContact=(req,res)=>{
-    
-}    
-}
+  exports.handlerContact =async (req, res) => {
+    const { error } = schema.validate(req.body);
+
+    const { message, fullname, email } = req.body;
+    await Contact.create({ fullname, email, message });
+  }
+};
